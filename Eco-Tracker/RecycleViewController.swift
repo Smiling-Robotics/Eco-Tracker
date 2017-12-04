1aff593b55b50cb0bd2f8d2f135fd4bacc097232
Christian <admin@iMac.local>
2017-12-03 23:28:15 -0700

    Update in list

--- Eco-Tracker/RecycleViewController.swift
+++ Eco-Tracker/RecycleViewController.swift
@@ -7,9 +7,39 @@
 //
 
 import UIKit
+var pinName = ""
+class RecycleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-class RecycleViewController: UIViewController {
-
+    let list = ["Milk", "Honey", "Tomatoes"]
+    var myIndex = 0
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(list.count)
+        
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default,  reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        myIndex = indexPath.row
+        debugPrint(myIndex)
+        if (myIndex == 2)
+        {
+            debugPrint("HEllo")
+            pinName = "A"
+            let viewController = storyboard?.instantiateViewController(withIdentifier: (pinName))
+            debugPrint(pinName)
+            self.navigationController?.pushViewController(viewController!, animated: true)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
